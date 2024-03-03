a5294e18c501f7bd4c7487b77901e44c0d859b8c
cam586 <c.mcgreavy@hotmail.com>
2024-03-03 12:23:15 +0100

    added current time logging for sync

--- arduino_read_serial.py
+++ arduino_read_serial.py
@@ -2,37 +2,72 @@ import serial
 import csv
 from datetime import datetime
 import os.path as  p
+import sys
+from time import time 
 
-serial_port = 'COM1'
-baud_rate = 9600
+serial_port = 'COM5'
+baud_rate = 115200
 
-# Open the serial port
-ser = serial.Serial(serial_port, baud_rate)  # Replace 'COM1' with the appropriate port and baud rate
+
+ 
+
+ser = serial.Serial(serial_port, baud_rate) 
 
 #build file dir
-save_dir = "C:/Users/USER/Desktop/Python/Serial" #any path
+save_dir = "C:/Users/lbernard/cernbox/WINDOWS/Desktop/Robotics/Quadruped_Models/Go1_test_data_ALICE"
 file_name = "output"
 file_app = datetime.now().strftime("%Y%m%d-%H%M%S")
 file_ext = ".csv"
-
 file_dir = p.join(save_dir, (file_name + "_" + file_app + file_ext))
-
-# Open the CSV file in append mode
 print ("Saving data to file: " + file_dir)
+
+
+i = 0
 with open(file_dir, 'a', newline='') as csvfile:
+
     writer = csv.writer(csvfile)
 
-    # Continuously read data from the serial port
-    try: 
-        while True:
-            line = ser.readline().decode().strip()  # Read a line from the serial port and decode it
-            writer.writerow([line])  # Write the line to the CSV file
-    except KeyboardInterrupt:
-        print ("Keyboard Interrupt. Exiting...")
-        ser.close()
-        csvfile.close()
-        print ("Serial port closed")
-        print ("CSV file closed")
-        print ("Exiting program")
-        exit()
+    while True:
 
+        i+=1
+
+        try:
+
+            if i > 13:
+                print(ser.readline().decode().strip())
+
+                curr_time_ms = int(time() * 1000) #current time since epoch in ms - divide by 1000 to get readable time
+
+                line = ser.readline().decode().strip()  # Read a line from the serial port and decode it
+
+                line = f"{curr_time_ms} {line}"
+
+                writer.writerow([line])  # Write the line to the CSV file
+
+        except KeyboardInterrupt:
+
+            print ("Keyboard Interrupt. Exiting...")
+
+            print ("Closing serial port...")
+            ser.close()
+            print ("Serial port closed")
+
+            print ("Closing CSV file...")
+            csvfile.close()
+            print ("CSV file closed")
+
+            print ("Exiting program")
+            sys.exit(1)
+
+
+
+
+        except:
+
+            print("Error reading serial port. Exiting.")
+
+            ser.close()
+
+            csvfile.close()
+
+            sys.exit(1)
